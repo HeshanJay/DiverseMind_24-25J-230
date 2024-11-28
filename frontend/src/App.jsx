d6b6912b9875ca87720c4cfb6ee4f4012b6ecc73
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2024-11-28 12:25:27 +0530

    set up tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import WritingCanvas from "./Components/WritingCanvas/WritingCanvas";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import LetterFormationTest from "./Pages/LetterFormationTest";
 
 const App = () => {
   return (
@@ -13,10 +11,6 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route
-            path="/letterformationtest"
-            element={<LetterFormationTest />}
-          />
         </Routes>
       </BrowserRouter>
     </div>
