cf272a6703978d73b97f04269e265d13314eee62
Sasrika Neewin <IT21302930@my.sliit.lk>
2024-11-29 22:28:53 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import AttentionReadingTest from "./Pages/ReadingDashboardpage";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          {/* Main Pages */}
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
+
+          {/* Reading Test Dashboard and Passages */}
+          <Route
+            path="/attentionreadingtest"
+            element={<AttentionReadingTest />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
