c23a3e194b6f28c9c369f8181c599906a440c533
Sasrika Neewin <IT21302930@my.sliit.lk>
2024-11-30 10:07:43 +0530

    updated quiz 2 for score calculation

--- frontend/src/Components/Passage3/Passage3.jsx
+++ frontend/src/Components/Passage3/Passage3.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Passage3.css"; // Additional styling
+import "./Passage3.css";
 import backgroundImage from "../../assets/background_images/back_img4.jpg";
 import { MdArrowBack, MdArrowForward } from "react-icons/md";
 
@@ -8,21 +8,24 @@ const Passage3 = ({ onPrevious, onNext }) => {
 
   const options = [
     "වගාවේ පිටුපස සැගවීම",
-    "විදුලිය වගේ පැනලා දුවීම",
+    "විදුලිය වගේ පැනලා දුවීම", // Correct answer is "B"
     "වගාව විනාශ කිරීම දිගටම කරමින් සිටීම",
     "හෙනේ මාමාට ප්‍රහාරක කිරීම",
   ];
+  const correctAnswer = "විදුලිය වගේ පැනලා දුවීම";
 
-  const handleOptionClick = (index) => {
-    setSelectedOption(index);
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  const handleNextClick = () => {
+    onNext(selectedOption === correctAnswer);
   };
 
   return (
     <div
       className="bg-cover bg-center w-screen h-screen flex justify-center items-center relative m-0 p-0"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-      }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="bg-white bg-opacity-90 p-8 rounded-xl shadow-2xl max-w-3xl text-center border-4 border-blue-400">
         <h1 className="text-3xl font-extrabold text-blue-600 mb-6">
@@ -32,9 +35,9 @@ const Passage3 = ({ onPrevious, onNext }) => {
           {options.map((option, index) => (
             <button
               key={index}
-              onClick={() => handleOptionClick(index)}
+              onClick={() => handleOptionClick(option)}
               className={`p-4 rounded-lg border-2 transition-transform duration-300 shadow-md hover:scale-105 ${
-                selectedOption === index
+                selectedOption === option
                   ? "bg-blue-500 text-white border-blue-700"
                   : "bg-white text-gray-800 border-gray-300"
               }`}
@@ -53,7 +56,7 @@ const Passage3 = ({ onPrevious, onNext }) => {
       </button>
 
       <button
-        onClick={() => onNext("B")} // Correct answer for Passage3 is "B"
+        onClick={handleNextClick}
         disabled={selectedOption === null}
         className={`absolute bottom-10 right-28 w-16 h-16 rounded-full shadow-lg flex justify-center items-center bg-gradient-to-r from-blue-400 to-green-500 hover:scale-110 transition-transform duration-300 ${
           selectedOption === null ? "opacity-50 cursor-not-allowed" : ""
