61807b2941d246e9c405ba0fc7664d6da8559b38
Heshan Jayasundara <heshanjayasundara218@gmail.com>
2024-12-04 01:09:54 +0530

    Complete punctuations test interfaces

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import VowelSymbolQ6 from "../Components/VowelSymbolTest/VowelSymbolQ6";
+import PunctuationsTestQ1 from "../Components/PunctuationsTest/PunctuationsTestQ1";
+import PunctuationsTestQ2 from "../Components/PunctuationsTest/PunctuationsTestQ2";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-lx font-bold text-red-500">Home Page</h1>
+      {/* <h1 className="text-lx font-bold text-red-500">Home Page</h1> */}
+      <VowelSymbolQ6 />
+      {/* <PunctuationsTestQ1 /> */}
+      {/* <PunctuationsTestQ2 /> */}
     </div>
   );
 };
