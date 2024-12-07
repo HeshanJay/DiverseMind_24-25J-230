52f2904ae56965a0a9fcaeabe3d1622c7ca8b68d
Sasrika Neewin <IT21302930@my.sliit.lk>
2024-12-07 15:00:03 +0530

    merge from dev branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-<<<<<<< HEAD
 import AttentionReadingTest from "./Pages/ReadingDashboardpage";
-=======
 import MathScreeningTest from "./Pages/math/MathScreeningTest";
 import WritingTest from "./Pages/WritingTest";
->>>>>>> 8d1dfc0da9c8d96dadbcd56f5eeea2ffbdf345c0
 
 const App = () => {
   return (
@@ -18,17 +15,14 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
-<<<<<<< HEAD
 
           {/* Reading Test Dashboard and Passages */}
           <Route
             path="/attentionreadingtest"
             element={<AttentionReadingTest />}
           />
-=======
           <Route path="/math" element={<MathScreeningTest />} />
           <Route path="/writingtest" element={<WritingTest />} />
->>>>>>> 8d1dfc0da9c8d96dadbcd56f5eeea2ffbdf345c0
         </Routes>
       </BrowserRouter>
     </div>
