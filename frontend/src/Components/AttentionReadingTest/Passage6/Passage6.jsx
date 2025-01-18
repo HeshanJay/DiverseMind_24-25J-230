5ab4ede488924aac6f088febc8db29afd39332fd
Sasrika Neewin <IT21302930@my.sliit.lk>
2025-01-18 14:55:27 +0530

    add camera off button

--- frontend/src/Components/AttentionReadingTest/Passage6/Passage6.jsx
+++ frontend/src/Components/AttentionReadingTest/Passage6/Passage6.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import backgroundImage from "../../../assets/background_images/back_img4.jpg";
 import { MdArrowBack, MdArrowForward } from "react-icons/md";
+import monkeyImage from "../../../assets/characters/Monkey.png";
+import bunnyImage from "../../../assets/characters/bunny.png";
+import foxImage from "../../../assets/characters/fox.png";
 
 const Passage6 = ({ onPrevious, onNext }) => {
   return (
@@ -8,16 +11,9 @@ const Passage6 = ({ onPrevious, onNext }) => {
       className="bg-cover bg-center w-screen h-screen flex justify-center items-center relative m-0 p-0"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="bg-white bg-opacity-90 p-8 rounded-xl shadow-2xl max-w-3xl text-center border-4 border-yellow-400">
-        <h1 className="text-3xl font-extrabold text-yellow-600 mb-6">
-          නැවත පැමිණි මියෝගේ වික්‍රමය
-        </h1>
-        <p className="text-lg text-gray-700 mb-6">
-          මියෝගේ වික්‍රමය ගැන පැමිණි ඔබේ අදහස් දක්වන්න. මෙම විෂය ගැන ඔබේ අදහස්
-          අපි හොඳින් ඇගයීමට බලාපොරොත්තු වෙමු.
-        </p>
-      </div>
-
+      <button className="absolute top-[40%] px-6 py-3 bg-gradient-to-r from-purple-400 to-pink-500 text-white text-lg font-bold rounded-full shadow-lg flex items-center space-x-2 hover:scale-105 hover:bg-gradient-to-l transition-transform duration-300">
+        Click to end
+      </button>
       <button
         onClick={onPrevious}
         className="absolute bottom-10 left-28 w-16 h-16 rounded-full shadow-lg flex justify-center items-center bg-gradient-to-r from-purple-400 to-pink-500 hover:scale-110 transition-transform duration-300"
@@ -31,6 +27,17 @@ const Passage6 = ({ onPrevious, onNext }) => {
       >
         <MdArrowForward size={40} color="white" />
       </button>
+
+      {/* Animated Images */}
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-[200%]">
+        <img src={foxImage} alt="Fox" className="w-28 monkey-animation" />
+      </div>
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-[60%]">
+        <img src={monkeyImage} alt="Monkey" className="w-28 monkey-animation" />
+      </div>
+      <div className="absolute bottom-10 left-1/2 transform translate-x-[60%]">
+        <img src={bunnyImage} alt="Bunny" className="w-28 monkey-animation" />
+      </div>
     </div>
   );
 };
