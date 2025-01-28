85baaf4933a3ce8b35ed2640e85cf85804fa31e4
NavodKCN <navod.kc@gmail.com>
2025-01-28 12:09:51 +0530

    add routes for the App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,12 +20,17 @@ import WritingTest from "./Pages/WritingTest";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
 
+import Dashboard from "./Components/Dashboard/Dashboard";
+import ResetPassword from "./Components/ResetPassword/ResetPassword";
+import StudentEntrance from "./Components/StudentEntrence/StudentEntrence";
+import HomePage from "./Pages/HomePage/home_page";
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route index element={<Home />} />
+        <Route index element={<HomePage />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/memory-tests" element={<MemoryTests />} />
@@ -50,6 +55,10 @@ const App = () => {
           <Route path="/writingtest" element={<WritingTest />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/student-entrance" element={<StudentEntrance />} />
+          <Route path="/home-page" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   </div>
