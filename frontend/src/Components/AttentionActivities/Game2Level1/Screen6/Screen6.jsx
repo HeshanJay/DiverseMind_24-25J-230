401584e0e4d54e51309de7a33b2c5e088d8eb4f8
Sasrika Neewin <IT21302930@my.sliit.lk>
2025-02-14 17:30:46 +0530

    update on tiles

--- frontend/src/Components/AttentionActivities/Game2Level1/Screen6/Screen6.jsx
+++ frontend/src/Components/AttentionActivities/Game2Level1/Screen6/Screen6.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import BackImage from "../../../../assets/background_images/AttentionGames/Game2/backimg1_game2.png";
 
 const Screen6 = () => {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-gray-500">Screen 6</h1>
+    <div
+      className="w-screen h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${BackImage})` }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+      <h1 className="relative text-xl font-bold text-gray-200">Screen 6</h1>
     </div>
   );
 };
